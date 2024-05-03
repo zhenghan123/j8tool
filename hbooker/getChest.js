8ade66b2ecc32323146802323c257522e47ff3bc
zhenghan123 <61129862+zhenghan123@users.noreply.github.com>
2024-05-03 15:28:56 +0800

    Update getChest.js

--- hbooker/getChest.js
+++ hbooker/getChest.js
@@ -1,61 +1,74 @@
-const axios = require('axios');
+const axios = require("axios");
 const CryptoJS = require("crypto-js");
 const config = require("./config.json");
 
 var sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
-// ...你的其它代码...
+const iv = CryptoJS.enc.Hex.parse('00000000000000000000000000000000')
 
-async function sendPushNotification(token, title, content) {
-  let response = await axios({
-      method: 'post',
-      url: 'http://pushplus.hxtrip.com/send',
-      data: {
-          token: token,
-          title: title,
-          content: content
-      }
-  });
-
-  console.log(response.data);
+const decrypt = function(data, key) {
+    key = CryptoJS.SHA256(key ? key : 'zG2nSeEfSHfvTCHy5LCcqtBbQehKNLXn')
+    const decrypted = CryptoJS.AES.decrypt(data, key, {
+        mode: CryptoJS.mode.CBC,
+        iv: iv,
+        padding: CryptoJS.pad.Pkcs7,
+    })
+    return decrypted.toString(CryptoJS.enc.Utf8)
 }
 
-// ...你的其它代码...
+function CGet(url, params) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            params += `&app_version=2.3.922&device_token=${device_token}&login_token=${token}&account=${username}`
+            url = "https://app.hbooker.com" + url;
+            const response = await axios.post(url, params);
+            let data = decrypt(response.data.trim());
+            const lastIndex = data.lastIndexOf("}");
+            data = data.substr(0, lastIndex + 1);
+            const json = JSON.parse(data);
+            resolve(json);
+        } catch (err) {
+            resolve({
+                tip: err.message ?? err.code,
+                errorwen: true
+            });
+        }
+    });
+}
 
 async function hbooker() {
-    let result = "【刺猬猫小说】：";
-    let msg = "" ;
-
-    // Inside your while loop:
+    let msg = ""
     while (true) {
-        let listData = await CGet("/chest/list_chest", `book_id=${bid}`);
-        let waittime2 = listData.error ? 30000 : (listData.data[0].left_time + 60);
-        let answer = await CGet("/reply/comment/add", `comment_id=${cid}&book_id=${bid}&shelf_id=&content=上`);
-
-        if(answer.code == 100000) {
-            let openRes;
-
-            if(answer.data.bonus_switch) {
-                console.log(">>去开宝箱");
-                openRes = await CGet("/chest/open_chest", `chest_id=${cid}`);
+        const message = await CGet("/reader/get_message_sys_list_by_type", "count=10&message_type=3&page=0")
+        if (message && message.data && message.data.message_sys_list) {
+            const cid = message.data.message_sys_list[0].chest_id
+            if (!cidArr.includes(cid)) {
+                let chestInfo = await CGet("/chest/get_chest_detail", `chest_id=${cid}&module=ouhuang&count=10&page=0`)
+                if (chestInfo.code == 100000) {
+                    chestInfo = chestInfo.data.chest_info
+                    const bid = chestInfo.book_info.book_id
+                    if (chestInfo.has_opened == 0) {
+                        await CGet("/bookshelf/favor", "shelf_id=&book_id=" + bid)
+                        let openRes = await CGet("/chest/open_chest", `chest_id=${cid}`)                        
+                        let errorwen = openRes.error
+                        await CGet("/bookshelf/favor", "shelf_id=&book_id=" + bid)                      
+                        while(errorwen) {
+                            openRes = await CGet("/chest/open_chest", `chest_id=${cid}`)
+                            await sleep(1000)
+                            if (!openRes.errorwen) errorwen = false
+                        }  
+                        if(openRes.code == 100000) 
+                            msg = openRes.data.item_name.match(/经验值/)?("经验值 * "+openRes.data.item_num) :(openRes.data.item_name +" * "+openRes.data.item_num      )                           
+                        else 
+                            msg = openRes.tip
+                        await CGet("/bookshelf/delete_shelf_book", "shelf_id=&book_id=" + bid)
+                    } else {
+                        cidArr.push(cid)
+                    }
+                } 
             }
-
-            // If the chest was opened successfully, send a push notification with the results:
-            if(openRes && openRes.code == 100000) {
-                msg = openRes.data.item_name.match(/经验值/)?("经验值 * "+openRes.data.item_num) :(openRes.data.item_name +" * "+openRes.data.item_num);
-                await sendPushNotification(config.token, 'Chest opened successfully', msg);
-            }
-
-            console.log(">>>>结果：" + msg);
-
-            if(errorwen) {
-                await CGet("/bookshelf/favor", "shelf_id=&book_id=" + bid);
-                await sleep(1000);
-                if (!openRes.error) errorwen = false;
-            }
-            // 你的while循环的代码...
         }
+        await sleep(1000 * 60 * 5)
     }
 }
 
-hbooker();
-//module.exports = hbooker()
+hbooker()
